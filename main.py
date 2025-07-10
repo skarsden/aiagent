99cb2c86a667cbb69992b38dcbb5776a7676115a
skarsden <zachfrancis98@gmail.com>
2025-07-09 21:06:24 -0400

    Added function to run python files - NOTE: THERE ARE NO SECURITY MEASURES IMPLEMENTED, BE CAREFUL WHAT YOU RUN

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 user_prompt = " ".join(sys.argv[1])
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -24,7 +25,9 @@ def main():
         verbose = True
     
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents = messages
+        model='gemini-2.0-flash-001',
+        contents = messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     print(response.text)
